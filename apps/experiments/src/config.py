b46939687cf767c2cfa8bebbe50631ada7230436
Alexis Gaziello <agaziello@bostonlighthouse.us>
2022-03-17 13:26:46 -0400

    config.py

--- apps/experiments/src/config.py
+++ apps/experiments/src/config.py
@@ -19,14 +19,9 @@ DATA_LAKE_DATABASE = os.environ["POSTGRESQL_DATABASE"]
 DATA_LAKE_USERNAME = "oc_bigdata"
 
 DATA_LAKE_BASE_URL = f"postgresql://{DATA_LAKE_USERNAME}:{DATA_LAKE_PASSWORD}@{DATA_LAKE_IP}:{DATA_LAKE_PORT}"
-DATA_LAKE_ENGINE = create_engine(f"{DATA_LAKE_BASE_URL}/{DATA_LAKE_DATABASE}", client_encoding='utf8')
 
+# TODO: uniformize all variable names
+RAW_DATA_ENGINE = create_engine(f"{DATA_LAKE_BASE_URL}/{DATA_LAKE_DATABASE}", client_encoding='utf8')
 
-def configure_mlflow_experiment(experiment_name: str, version: str):
-    set_tracking_uri(f"http://{MLFLOW_IP}:{MLFLOW_PORT}")
-    set_experiment(experiment_name)
-    start_run(run_name=version)
-
-
-def save_model(mlflow_model: Any, model, model_name: str):
-    mlflow_model.log_model(model, model_name)
+# TODO: this should point to the machine learning database
+ML_DATA_ENGINE = create_engine(f"{DATA_LAKE_BASE_URL}/{DATA_LAKE_DATABASE}", client_encoding='utf8')
